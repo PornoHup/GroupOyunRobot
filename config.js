e8ae686b8d258217525008f8f7dfc0fc2b4db711
PornoHup <107538335+PornoHup@users.noreply.github.com>
2022-06-20 00:46:58 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	token: "5252626888:AAFWG22kR-vP_TFpjmVg0CnZyWon_XSpWYc",
-	botId: 5252626888, //first numbers of BOT_TOKEN
+	token: "5537227242:AAEXhgEuzJKUEgv7NQz0fgsci51WO0lldRk",
+	botId: 5537227242, //first numbers of BOT_TOKEN
 	rounds: 20, //rounds amount
 	waitDelay: 12000, //1 round duration
-	timerSteps: 5, //timer indicators
+	timerSteps: 6, //timer indicators
 }
